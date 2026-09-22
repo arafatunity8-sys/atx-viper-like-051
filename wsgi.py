df36312a39015177451c35bb2f528a63e682b610
arafatunity8-sys <arafatunity8@gmail.com>
2026-09-23 03:23:51 +0600

    chore: sync files via GitMultiSync

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
+import os
 from app import app
 
-# WSGI helper
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port)
+
